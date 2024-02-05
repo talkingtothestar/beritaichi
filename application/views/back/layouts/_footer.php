1e1632899b27e6a704aa73277db00fa8d6aaa41f
unknown <mbibrahim266@gmail.com>
2024-02-05 11:31:37 +0700

    first commit

---
+++ application/views/back/layouts/_footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+   <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+      <span>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Repost by <a href='https://sharecodingan.blogspot.com/' title='sharecodingan.blogspot.com' target='_blank'>ShareCodingan.</a>
+		</span>
+      </div>
+   </div>
+</footer>
